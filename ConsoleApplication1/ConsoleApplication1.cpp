f4f39ec263e3541b3ed42ebbd44d26142c996c94
Sonic-Iron <sam@thehewittfamily.com>
2023-11-23 23:30:22 +0000

    deleted all the bad code

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,25 +5,10 @@
 #include <vector>
 #include <string>
 
-struct vertex {
-    std::string name;
-    vertex(std::string s) : name(s) {}
-    typedef std::pair <int, vertex*> ver;
-    std::vector <ver> adj;
-    
-};
 
 
-class Graph {
-
-    void addVertex(const std::string name){
-
-        return;
-    }
-
-};
 
 int main()
 {
-    
+
 }
