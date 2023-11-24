7137aa7829faf301a8e1e36d9a3c5f2f01942b51
Sonic-Iron <sam@thehewittfamily.com>
2023-11-24 01:11:04 +0000

    it's a good start

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,26 @@
 
 #include<iostream>
+#include<map>
+#include<vector>
+#include<pair>
+
+
+struct vertex {
+	int ID;
+	std::vector<std::pair<vertex, float>> edges;
+	vertex* pre_node;
+	bool explored;
+};
+
+void AddNode() {
+
+}
+
+void AddEdge() {
+
+}
 
 int main() {
-
+	std::vector<vertex> graph;
+	
 }
