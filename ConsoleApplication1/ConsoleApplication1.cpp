80ce886b6ecc28def581c3f2b24eb775ba41ad5f
Sonic-Iron <sh2611@york.ac.uk>
2023-11-26 23:16:36 +0000

    restarting this later

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,25 +1,37 @@
-
 #include<iostream>
 #include<map>
 #include<vector>
 
 
+
 struct vertex {
 	int ID;
-	std::vector<std::pair<vertex, float>> edges;
-	vertex* pre_node; //shared_ptr ?
-	bool explored;
+	struct edge {
+		int destination;
+		float cost;
+	}
+	std::vector<edge> edges;
+	bool explored = false;
 };
 
-void AddNode() {
-
+static void AddNode(std::vector graph, int nodeID) {
+	std::unique_ptr<vertex> node = 
+	node.ID = nodeID;
+	graph.push_back(node);
 }
 
-void AddEdge(vertex node1, vertex node2, float cost) {
-
+static void AddEdge(std::vector graph, vertex node1, vertex node2, float cost) {
+	
 }
 
 int main() {
+	int number_nodes = 0;
 	std::vector<vertex> graph;
+	
+	AddNode(graph, ++number_nodes);
+
+	std::cout << *graph[0].ID << std::endl;
+
+
 	
 }
