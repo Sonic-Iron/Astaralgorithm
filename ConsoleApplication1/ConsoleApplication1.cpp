42f666b7e861e409d0da3002dd7158a36922e5c9
Sonic-Iron <sam@thehewittfamily.com>
2023-12-24 01:10:35 +0000

    Revert "big changes"
    This reverts commit 96626b0babdf6a47e7eddd8df5fe3f0c147f8fee.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,71 +1,55 @@
-#include <array>
-#include <iostream>
-#include <map>
-#include <vector>
+#include<iostream>
+#include<map>
+#include<vector>
 
 
-#define DIR_N 0
-#define DIR_NE 1
-#define DIR_E 2
-#define DIR_SE 3
-#define DIR_S 4
-#define DIR_SW 5
-#define DIR_W 6
-#define DIR_NW 7
-#define DIR_COUNT 8
-
-
-
-size_t idx2d(const size_t cols, const size_t x, const size_t y)
-{
-
-	return (cols * y) + x;
-}
-
-struct node
-{
-	int ID;
-	int type;
-	//std::array<float, DIR_COUNT> edges {};
+struct edge {
+	int nodeTo;
+	float cost;
 };
 
-std::vector<node> create_area(const char area_type, const int area_side_length)
-{
+struct node {
+	int ID;
+	std::string symbol;
+	std::vector<edge>;
+};
 
-	std::vector<node> nodes;
-	//nodes.reserve(area_side_length * area_side_length); //reserves uninitialised memory space of node type (expects nodes)
-	
-	/*
-	nodes.push_back({ 1,1 }); //can do this because the vector expects node type
-	nodes.push_back({ 2,2 });
-	nodes.erase(nodes.begin()); //gets rid of the 0th element, reallocates the positions of all other elements so is inefficient.
-	nodes.erase(nodes.begin()); //same thing but for the second element created as the second was moved to the 0th posiiton
-	*/
-	for (int i = 0; i < (area_side_length * area_side_length); i++)
-	{
-		nodes.push_back({ i, 0 });
+std::string create_area(std::string& area, const std::string area_type, const int area_size) {
+	std::array<std::array<std::string, area_size>, area_size> area;
+	if (area_type = 'r') {
+
 	}
-	//std::cout << nodes.size() << ' ' << nodes.at(0).ID << std::endl; //gets '1' as the first pushback is 1 for the ID. Checks my code is working
+	else if (area_type = 'h') {
 
-	return nodes;
+	}
+	else if (area_type = 'u') {
 
+	}
+	else {
+		return '0';
+	}
 }
 
 
-void run_ASA()
-{
+void run_ASA() {
 
 }
 
-int main()
-{
-	char area_type;
+int pickpoint()
+
+int main() {
+	string area_type;
 	int area_size;
-	const int num_people = 10;
-
-	std::cout << "Type of area (r for rural, s for suburban, and u for urban)" << std::endl;
+	std::cout << "Type of area" << std::cout;
 	std::cin >> area_type;
-	std::cout << "Size of the area in intergers" << std::endl;
+	std::cout << "Size of area in integer" << std::cout;
 	std::cin >> area_size;
-	std::vector<node> nodes = create_area(area_type, area_size);	
+	area = create_area(area, area_type, area_size);
+	if (area = '0') {
+		return 1;
+	}
+	else {
+		run_ASA(num_people, area);
+	}
+	
 }
