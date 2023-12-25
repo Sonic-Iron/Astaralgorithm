5965acae1d12525c5b4a01654712bc58e60b90e4
Sonic-Iron <sam@thehewittfamily.com>
2023-12-25 02:51:39 +0000

    quick update, nothing major. Changing idx2d code

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,24 +16,42 @@
 
 
 
-size_t idx2d(const size_t cols, const size_t x, const size_t y)
+int idx2d(const int current_ID;  const int row_size, const int x, const int y) //returns the position of a node converted from a 2d space to a 1d vector
 {
+	if (y - 1 < 0 || y + 1 > row_size) 
+	{
+		return -1;
+	}
+	if (x - 1 < 0 || x + 1 > row_size)
+	{
+		return -1;
+	}
 
-	return (cols * y) + x;
+	// add here the new ID
 }
 
 struct node
 {
-	int ID;
-	int type;
-	//std::array<float, DIR_COUNT> edges {};
+	const int ID;
+	const int type;
+	const char symbol;
+	std::array<float, DIR_COUNT> edges;
 };
+
+std::vector<node> create_edges(std::vector<node> nodes) //use inline here as it could be put into the create_area function, it doesn't return anything
+{ 
+	for (node node : nodes)
+	{
+		
+	}
+	return nodes;
+}
 
 std::vector<node> create_area(const char area_type, const int area_side_length)
 {
 
 	std::vector<node> nodes;
-	//nodes.reserve(area_side_length * area_side_length); //reserves uninitialised memory space of node type (expects nodes)
+	nodes.reserve(area_side_length * area_side_length); //reserves uninitialised memory space of node type (expects nodes)
 	
 	/*
 	nodes.push_back({ 1,1 }); //can do this because the vector expects node type
@@ -41,11 +59,12 @@ std::vector<node> create_area(const char area_type, const int area_side_length)
 	nodes.erase(nodes.begin()); //gets rid of the 0th element, reallocates the positions of all other elements so is inefficient.
 	nodes.erase(nodes.begin()); //same thing but for the second element created as the second was moved to the 0th posiiton
 	*/
+	//std::cout << nodes.capacity() << nodes.size() << std::endl; Returns 90, this is because of the reserve function above. 
 	for (int i = 0; i < (area_side_length * area_side_length); i++)
 	{
-		nodes.push_back({ i, 0 });
+		nodes.push_back({ i, 0, {} });
 	}
-	//std::cout << nodes.size() << ' ' << nodes.at(0).ID << std::endl; //gets '1' as the first pushback is 1 for the ID. Checks my code is working
+	//std::cout << nodes.capacity() << nodes.size() << std::endl; Returns 99 
 
 	return nodes;
 
