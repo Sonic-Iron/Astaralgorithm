01fcc3ea653c6a394881fd1756dbe4d8daa98973
Sonic-Iron <sam@thehewittfamily.com>
2024-01-18 02:10:07 +0000

    finsihed idx2d function

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -14,26 +14,34 @@
 #define DIR_NW 7
 #define DIR_COUNT 8
 
+
 struct node
 {
 	const int ID;
 	const int type;
-	const char symbol;
 	std::array<float, DIR_COUNT> edges;
 };
 
 
-int idx2d(const int current_ID, const int delta_x, const int delta_y) //returns the position of a node converted from a 2d space to a 1d vector
+int idx2d(const int current_ID, const int delta_x, const int delta_y, const int length_x, const int length_y) //returns the position of a node converted from a 2d space to a 1d vector
 {
-	
-	return 1;
+	if(current_ID % length_y - delta_x < 0 || current_ID % length_y + delta_x > length_x) { //off the sides
+		return current_ID;
+	}
+	if (current_ID + length_x * delta_y > 0 || current_ID - length_x * delta_y > length_x * length_y) {//off the top/bottom
+		return current_ID;
+	}
+	return current_ID + length_x * delta_y + delta_x;
 }
 
-std::vector<node> create_edges(std::vector<node>& nodes, const int length_x, const int length_y) //use inline here as it could be put into the create_area function, it doesn't return anything
+std::vector<node> create_edges(std::vector<node>& nodes, const int length_x, const int length_y) //use inline here as it could be put into the create_area function?, it doesn't return anything
 {
 	for (node node : nodes)
 	{
+		if()
+		{
 
+		}
 	}
 	return nodes;
 }
@@ -43,18 +51,10 @@ std::vector<node> create_area(const char area_type, const int length_x, const in
 	std::vector<node> nodes;
 	nodes.reserve(length_x * length_y); //reserves uninitialised memory space of node type (expects nodes)
 
-	/*
-	nodes.push_back({ 1,1 }); //can do this because the vector expects node type
-	nodes.push_back({ 2,2 });
-	nodes.erase(nodes.begin()); //gets rid of the 0th element, reallocates the positions of all other elements so is inefficient.
-	nodes.erase(nodes.begin()); //same thing but for the second element created as the second was moved to the 0th posiiton
-	*/
-	//std::cout << nodes.capacity() << nodes.size() << std::endl;
 	for (int i = 0; i < (length_x * length_y); i++)
 	{
-		nodes.push_back({ i, 0, {} });
+		nodes.push_back({ i, 0, {}});
 	}
-	//std::cout << nodes.capacity() << nodes.size() << std::endl; These are different and reflect the fact that the vector is now filled.
 	nodes = create_edges(nodes, length_x, length_y); //do i need to return nodes here?
 
 	return nodes;
