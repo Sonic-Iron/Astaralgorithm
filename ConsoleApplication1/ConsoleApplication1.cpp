406b2529f2800e11a5601aadfba8d80533cbc805
Sonic-Iron <sam@thehewittfamily.com>
2024-01-26 14:30:09 +0000

    added case for directional movement

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,36 +12,59 @@
 #define DIR_SW 5
 #define DIR_W 6
 #define DIR_NW 7
-#define DIR_COUNT 8
 
 
 struct node
 {
 	const int ID;
 	const int type;
+	const char symbol;
 	std::array<float, DIR_COUNT> edges;
 };
 
 
-int idx2d(const int current_ID, const int delta_x, const int delta_y, const int length_x, const int length_y) //returns the position of a node converted from a 2d space to a 1d vector
+int idx2d(const int current_loc, const int delta_x, const int delta_y, const int length_x, const int length_y) //returns the position of a node converted from a 2d space to a 1d vector, y is positive in the positive direction. North is positive
+
 {
-	if(current_ID % length_y - delta_x < 0 || current_ID % length_y + delta_x > length_x) { //off the sides
-		return current_ID;
+	if(current_loc % length_y + delta_x < 0 || current_loc % length_y + delta_x > length_x) { //check for off the sides
+		return current_loc;
 	}
-	if (current_ID + length_x * delta_y > 0 || current_ID - length_x * delta_y > length_x * length_y) {//off the top/bottom
-		return current_ID;
+	if (current_loc + (length_x * delta_y) < 0 || current_loc + length_x * delta_y > (length_x * length_y) { //check for off the top/bottom
+		return current_loc;
 	}
-	return current_ID + length_x * delta_y + delta_x;
+	return current_ID + (length_x * delta_y) + delta_x;
+}
+int idx2d(const int current_loc, const int direction, const int length_x, const int length_y)
+{
+	switch (direction)
+	{
+	case 0:
+		return idx2d(current_loc, 0, 1, length_x, length_y)
+	case 1:
+		return idx2d(current_loc, 1, 1, length_x, length_y)
+	case 2:
+		return idx2d(current_loc, 1, 0, length_x, length_y)
+	case 3:
+		return idx2d(current_loc, 1, -1, length_x, length_y)
+	case 4:
+		return idx2d(current_loc, 0, -1, length_x, length_y)
+	case 5:
+		return idx2d(current_loc, -1, -1, length_x, length_y)
+	case 6:
+		return idx2d(current_loc, -1, 0, length_x, length_y)
+	case 7:
+		return idx2d(current_loc, -1, 1, length_x, length_y)
+	default:
+		return current_loc;
+	}
 }
 
 std::vector<node> create_edges(std::vector<node>& nodes, const int length_x, const int length_y) //use inline here as it could be put into the create_area function?, it doesn't return anything
 {
-	for (node node : nodes)
+	const int directions[3]{ 1,2,3 };
+	for (int dir : directions)
 	{
-		if()
-		{
 
-		}
 	}
 	return nodes;
 }
@@ -53,7 +76,7 @@ std::vector<node> create_area(const char area_type, const int length_x, const in
 
 	for (int i = 0; i < (length_x * length_y); i++)
 	{
-		nodes.push_back({ i, 0, {}});
+		nodes.push_back({ i, 0, '.', {}});
 	}
 	nodes = create_edges(nodes, length_x, length_y); //do i need to return nodes here?
 
@@ -72,7 +95,13 @@ int main()
 	char area_type;
 	int length_x;
 	int length_y;
-	const int num_people = 10;
+	const int num_people {10};
+	
+	int integers[3] {1,2,3};
+	
+	for (int a : integers) {
+		std::cout << a << std::endl;
+	}
 
 	std::cout << "Type of area (r for rural, s for suburban, and u for urban)" << std::endl;
 	std::cin >> area_type;
