be50453c9fc454a0a31483ed07ac38cd2d82066f
Sonic-Iron <sh2611@york.ac.uk>
2024-01-30 02:16:20 +0000

    worked on create_edges for a bit

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -22,19 +22,19 @@ struct node
 	const int type;
 	const char symbol;
 	bool visited = false;
-	std::array<float, DIR_COUNT> edges;
+	std::array<std::pair<int,int>, DIR_COUNT> edges;
 };
 
 int idx2d(const int current_loc, const int delta_x, const int delta_y, const int length_x, const int length_y) //returns the position of a node converted from a 2d space to a 1d vector, y is positive in the positive direction. North is positive
 {
 	if(current_loc % length_y + delta_x < 0 || current_loc % length_y + delta_x > length_x) 
 	{ //check for off the sides
-		return current_loc;
+		return -1;
 	}
 	if(current_loc + length_x * delta_y < 0 || current_loc + length_x * delta_y > length_x * length_y)
 	{ //check for off the top/bottom
-		return current_loc;
-	}
+		return -1;
+	}// Should I check for out of bounds here or use a try/catch?
 	return (current_loc + (length_x * delta_y) + delta_x);
 }
 
@@ -66,17 +66,22 @@ int idx2d(const int current_loc, const int direction, const int length_x, const 
 
 int calculate_edge_cost(const int type1, const int type2)
 {
-
 	return 1; //will update this later, all types of path will have the same length for now
 }
 
 std::vector<node> create_edges(std::vector<node>& nodes, const int length_x, const int length_y) //use inline here as it could be put into the create_area function?, it doesn't return anything
 {
-	for (int location = 0; location<nodes.size(); location++)
+	for (int location = 0; location < nodes.size(); location++)
 	{
-		for (int i = 0; i < 8; i++)
+		std::cout << location << std::endl;
+		for (int direction = 0; direction < 8; direction++)
 		{
-			nodes.at(location).edges.at(i) = 
+			int new_location = idx2d(location, direction);
+			if (new_location == -1)
+			{
+				return std::vector<node>(); //returns an anonymous object? there is no lvalue here so is it safe?
+			}
+			nodes.at(location).edges.at(location, direction, length_x, length_y) = calculate_edge_cost(nodes[location].type, nodes[new_location].type);
 		}
 	}
 	return nodes; //since nodes is a reference do I have to return it here? I could have a void function as all this does is modify the nodes vector?
@@ -114,6 +119,8 @@ int main()
 	std::cin >> length_x >> length_y;
 	std::cout << area_type << length_x << length_y << std::endl;
 	std::vector<node> nodes = create_area(area_type, length_x, length_y);
+
+	return 0;
 }
 
 
