96f87372a73b0e42e63e3e8baae1a63ed60cc67a
Sonic-Iron <sam@thehewittfamily.com>
2024-03-07 15:21:33 +0000

    completed edges, started create_area and ASA function

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -21,8 +21,8 @@ struct node
 	//When to put a constructor here?
 	const size_t location_index;
 	size_t pre_location_index;
+	float movement_cost;
 	const int type;
-	const char symbol;
 	bool visited;
 	std::array<std::pair<int,int>, DIR_COUNT> edges;
 };
@@ -66,12 +66,6 @@ int idx2d(const int current_loc, const int direction, const int length_x, const 
 	}
 }
 
-int calculate_edge_cost(const int type1, const int type2)
-{
-	
-	return 1; //will update this later, all types of path will have the same length for now
-}
-
 std::vector<node> create_edges(std::vector<node>& nodes, const int length_x, const int length_y) //use inline here as it could be put into the create_area function?, it doesn't return anything
 {
 	for (int location = 0; location < nodes.size(); location++) //use int here because they will be used for 'real' values?
@@ -82,16 +76,20 @@ std::vector<node> create_edges(std::vector<node>& nodes, const int length_x, con
 			int new_location = idx2d(location, direction, length_x, length_y); //returns an int until it knows it is a safe value to cast to size_t
 			if (new_location < 0)
 			{
-				nodes.at(location).edges.at(direction) = (std::numeric_limits<int>::max(), new_location); //I assume the complier will optimise and change this to -1? Also is there a way to do this without using another include? Does it matter?
+				nodes.at(location).edges.at(direction) = (new_location, std::numeric_limits<int>::max()); 
 			}
 			else
 			{
-				nodes.at(location).edges.at(direction) = (calculate_edge_cost(nodes.at(location).type, nodes.at(new_location).type), new_location);
+				nodes.at(location).edges.at(direction) = (new_location, nodes.at(new_location).movement_cost);
 			}
-
 		}
 	}
 	return nodes; //since nodes is a reference do I have to return it here? I could have a void function as all this does is modify the nodes vector?
+}
+
+std::vector<node> create_terrain(std::vector<node>& nodes, const int length_x, const int length_y, const char area_type)
+{
+
 }
 
 std::vector<node> create_area(const char area_type, const int length_x, const int length_y)
@@ -104,14 +102,15 @@ std::vector<node> create_area(const char area_type, const int length_x, const in
 		nodes.push_back({i, i, 0, '.', false, {}}); //TODO: sort this out lol //also instead of pushback use assignment, i.e. nodes[i]/nodes.at(i) = {i, ...}
 		//create the areas here I guess
 	}
+	nodes = create_terrain(nodes, length_x, length_y, area_type);
 	nodes = create_edges(nodes, length_x, length_y); //do i need to return nodes here?
-
 	return nodes;
 }
 
-void run_ASA()
+std::vector<int> run_ASA(std::vector<node>& nodes, int first_node, int target_node)
 {
 
+	return visited_nodes
 }
 
 int main()
